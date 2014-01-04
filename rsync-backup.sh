905d55be72091cf242e84e1e5600a4e12cf04a0d
Mario Marić <git@mariomaric.net>
2014-01-04 14:27:27 +0100

    Added backup script.

---
+++ rsync-backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Usage: backup [OPTION...]
+
+# http://www.dwheeler.com/essays/filenames-in-shell.html
+IFS="$(printf '\n\t')"
+
+# Arguments
+src="/home/mama/"
+dest="/media/DATAMM01/mama-lap-backup/"
+include="/home/mama/.rsync-backup-folder-list-include.txt"
+exclude="/tmp/rsync-backup-folder-list-exclude.txt"
+log="/tmp/rsync-backup.log"
+options="-avh"
+
+# Aplication
+if [ -d "$src" -a -d "$dest" -a -f "$include" ]
+then
+    # Gather files and folders to exclude
+    ls -AlF "$src" | awk '{print ($9)}' | grep -v -f "$include" - > "$exclude"
+    
+    # Backup
+    rsync $options $@ --exclude-from="$exclude" "$src" "$dest" &> "$log"
+else
+    echo "Something is wrong!"
+fi
